8df3b06c3a20ac25762c1f37171158cffe7e0bf9
youngjae_son <youngjae_son@tmax.co.kr>
2022-07-20 09:35:09 +0900

    Get video file

---
+++ my-new-project/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+const port = 3000;
+
+app.get("/video", (req, res) => {
+    //res.send("Hello World!");
+    const path = "/home/yjson/Videos/SampleVideo_1280x720_1mb.mp4";
+    fs.stat(path, (err, stats) => {
+        if (err) {
+            console.log("An error occurred ");
+            res.sendStatus(500);
+            return;
+        }
+
+        res.writeHead(200, {
+            "Content-Length": stats.size,
+            "Content-Type": "video/mp4",
+        });
+        fs.createReadStream(path).pipe(res);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
